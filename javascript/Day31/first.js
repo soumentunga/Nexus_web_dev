beedebc62676df7164746d1f7a6cb7343f0bc9a2
[SoumenTunga] <soumentungamail@gmail.com>
2025-03-19 00:15:51 +0530

    async and wait example code write

---
+++ javascript/Day31/first.js
@@ -0,0 +1,77 @@
+const cart = ["Pizza","Coke", "sandwich"];
+
+function placeOrder(cart){
+    console.log("Talking with Domino's");
+     
+    const pr = new Promise(function(resolve,reject){
+
+        setTimeout(()=>{
+            
+           const food_available = true;
+            if(food_available){
+            console.log("Order Placed Succesfully");
+            const order = {orderId: 221 , food: cart , restaurant: "Dominos", location:"Dwarka"}
+            resolve(order);
+            }
+            else{
+                reject("Items Out of Stocks");
+            }
+           },2000)
+
+    })
+
+
+    return pr;
+}
+
+function preparingOrder(order){
+    console.log("Pizza preparation started....");
+
+    const pr = new Promise(function(resolve,reject){
+
+        setTimeout(()=>{
+            console.log("Pizza preparation Done");
+            const foodDetails = {token: 12 , restaurant: order.restaurant, location: order.location};
+            resolve(foodDetails);
+           },5000)
+    })
+
+    return pr;
+};
+
+function pickupOrder(foodDetails){
+    console.log("Reaching restaurant for picking order")
+    
+    const pr = new Promise(function(resolve,reject){
+        setTimeout(()=>{
+            console.log("Order picked up by Delivery Boy");
+            const droplocation = foodDetails.location;
+            resolve(droplocation);
+           },3000)
+
+    })
+    
+    return pr;
+    
+}
+
+function deliverOrder(droplocation){
+    console.log("Delivery boy on the way");
+
+    setTimeout(()=>{
+        console.log("Order Delivered succesfully");
+    },5000)
+}
+
+//await will work only inside the async function without async function the await does not exits.
+
+async function Zomato() {
+
+    const order = await placeOrder(cart)
+    const foodDetails =await preparingOrder(order)
+    const droplocation =await  pickupOrder(foodDetails)
+    deliverOrder(droplocation) 
+}
+
+
+Zomato();
