1128ffa0c486d135e7b2972e824e862a404f3f9c
[SoumenTunga] <soumentungamail@gmail.com>
2025-03-19 01:59:25 +0530

    Some more practice updated

--- javascript/Day31/first.js
+++ javascript/Day31/first.js
@@ -65,16 +65,16 @@ function deliverOrder(droplocation){
 
 //await will work only inside the async function without async function the await does not exits.
 
-// async function Zomato() {
+async function Zomato() {
 
-//     const order = await placeOrder(cart)
-//     const foodDetails =await preparingOrder(order)
-//     const droplocation =await  pickupOrder(foodDetails)
-//     deliverOrder(droplocation) 
-// }
+    const order = await placeOrder(cart)
+    const foodDetails =await preparingOrder(order)
+    const droplocation =await  pickupOrder(foodDetails)
+    deliverOrder(droplocation) 
+}
 
 
-// Zomato();
+Zomato();
 
 
 //--------Comparision before and After async and await introduced --------- //
@@ -82,29 +82,29 @@ function deliverOrder(droplocation){
 
 //before async await =>
 
-// const p1 = new Promise((resolve,reject)=>{
+const p1 = new Promise((resolve,reject)=>{
 
-//     setTimeout(()=>{
-//         resolve("The Task is Resolve")
-//     },3000)
-// })
+    setTimeout(()=>{
+        resolve("The Task is Resolve")
+    },3000)
+})
 
-// //p1.then((response)=>console.log(response))
+p1.then((response)=>console.log(response))
 
-// //---------------------------------------------------------------------------
+//---------------------------------------------------------------------------
 
-// //after async and await 
+//after async and await 
 
-// async function greet() {
+async function greet() {
 
-//     const data1= await p1;
-//     console.log(data1)
-//     const data2= await p1;
-//     console.log(data2)
-// }
-// //after print data1 the data2 print without taking time is because the promise is already resolved so it's print directely it's the original concept all other concept avliable on internet is baseless (example:parallel  executing type of concept avliable on internet)
+    const data1= await p1;
+    console.log(data1)
+    const data2= await p1;
+    console.log(data2)
+}
+//after print data1 the data2 print without taking time is because the promise is already resolved so it's print directely it's the original concept all other concept avliable on internet is baseless (example:parallel  executing type of concept avliable on internet)
 
-//  greet();
+ greet();
 
 
 
@@ -133,3 +133,102 @@ async function greet() {
     }
 
 greet();
+
+
+//differance between .then and async - await method
+
+const p1 = new Promise((resolve,reject)=>{
+
+    setTimeout(()=>{
+        resolve("First Promise Resolved ")
+    },8000)
+})
+
+const p2= new Promise((resolve,reject)=>{
+
+    setTimeout(()=>{
+        resolve("Second Promise Resolved")
+    },5000)
+})
+
+async function greet() {
+
+        const data1= await p1;
+        console.log(data1)
+        const data2= await p2;
+        console.log(data2)
+    }
+
+greet();
+
+//here after p1 then p2 data print because of await . after await p1 then the next statement execute which is await p2.
+
+
+
+//by not using async await (using old .then method)
+
+const p1 = new Promise((resolve,reject)=>{
+
+    setTimeout(()=>{
+        resolve("First Promise Resolved ")
+    },8000)
+})
+
+const p2= new Promise((resolve,reject)=>{
+
+    setTimeout(()=>{
+        resolve("Second Promise Resolved")
+    },5000)
+})
+
+p1.then(value=>console.log(value))
+p2.then(value=>console.log(value))
+
+//here value of p2 execute first because here the time of p2 is less so p2 is execute first in the previous case it's don't happens because it's wait for previous p1 completition. here(.then method ) no waiting concept is there.which come first executing first. 
+
+
+//One more Example below =>
+
+function test1(){
+
+    const p1 = new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("First Promise resolved");
+        },5000)
+    })
+
+    return p1;
+
+}
+
+
+function test2(){
+    
+    const p2 = new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Second Promise resolved");
+        },5000)
+    })
+
+    return p2;
+
+}
+
+async function greet() {
+
+        const data1= await test1();
+        console.log(data1)
+        const data2 = await test2();
+        console.log(data2)
+    }
+
+greet();
+//here after test1 print wait then test2 will print
+
+//Important Note :: async function always return promise. if we return nothing then it make undefined into promise .here in below example  async make Soumen into  promise. 
+async function meet() {
+    
+    return "Soumen"
+}
+
+meet().then(value=>console.log(value))
