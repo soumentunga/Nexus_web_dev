b2155b33200ae918e3ef4ce5ce05dcbaf737fdd9
[SoumenTunga] <soumentungamail@gmail.com>
2025-04-01 00:18:39 +0530

    Day 2 Totally Completed

--- Typescript/Day2/first.js
+++ Typescript/Day2/first.js
@@ -17,3 +17,39 @@ if (typeof val2 === 'string')
     console.log(val2.toUpperCase());
 if (typeof val2 === 'number')
     console.log(val2.toFixed(2));
+//non - premitive data type 
+let arr = [2, 3, 4, 6, 7];
+let arr2 = [2, 3, 5, 48, 9];
+let arr3 = ["Soumen", 2, 6, "Mohan"];
+arr3.push(23);
+let arr4 = [3, 5, "Soumen", false, 23];
+//touple(same as array but fixed size) 
+let touple = ["Soumen", 21];
+//Javascrpt :JIT = JUST IN TIME (compiler + Interpreter)
+//What is platform independent or what is platform Dependent language
+//Typescript : Transpiler(convert a language to another language)
+//Object
+//inline method
+let obj1 = {
+    name: "Soumen Tunga",
+    age: 20,
+    gender: "male"
+};
+//second method
+let person;
+person = {
+    name: "Soumen Tunga",
+    age: 20,
+    balance: 324
+};
+let c1 = {
+    name: "Soumen Tunga",
+    age: 20,
+    id: "CSE22023"
+};
+let obj3 = {
+    name: "Soumen",
+    age: 21,
+    position: "manager",
+    id: "CSE22023"
+};
--- Typescript/Day2/first.ts
+++ Typescript/Day2/first.ts
@@ -23,3 +23,83 @@ console.log(val2.toUpperCase())
 if (typeof val2==='number')
 console.log(val2.toFixed(2))
 
+//non - premitive data type 
+
+let arr:number[] = [2,3,4,6,7];
+
+let arr2 = [2,3,5,48,9]
+
+let arr3:(string | number)[]=["Soumen",2,6,"Mohan"];
+arr3.push(23);
+
+let arr4:(string | number | boolean) []=[3,5,"Soumen",false,23];
+
+
+//touple(same as array but fixed size) 
+
+let touple:[string,number]=["Soumen",21];
+
+
+
+
+//Javascrpt :JIT = JUST IN TIME (compiler + Interpreter)
+//What is platform independent or what is platform Dependent language
+
+//Typescript : Transpiler(convert a language to another language)
+
+
+//Object
+//inline method
+let obj1:{name:string,age:number,gender:string} ={
+    name:"Soumen Tunga",
+    age:20,
+    gender:"male"
+}
+
+//second method
+
+let person:{name:string,age:number,balance:number};
+
+person={
+    name:"Soumen Tunga",
+    age:20,
+    balance:324
+};
+
+//using type alises
+
+type customer={
+    name:string,
+    age:number,
+    id:string
+}
+
+let c1:customer={
+    name:"Soumen Tunga",
+    age:20,
+    id:"CSE22023"
+}
+
+//using interface(this is most useful then type)
+
+
+interface admin{
+    name:string,
+    age:number,
+    position:string
+}
+
+interface admin{
+    id:string
+}
+
+let obj3:admin= {
+    name:"Soumen",
+    age:21,
+    position:"manager",
+    id:"CSE22023"
+}
+//homework
+//interface vs type
+//Function
+//classes 
