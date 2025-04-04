f4d7bb9203bda7f4601a8f3374bc28559046eb07
[SoumenTunga] <soumentungamail@gmail.com>
2025-04-04 14:03:43 +0530

    Day 2 Completed

---
+++ React/Day2/app.js
@@ -0,0 +1,52 @@
+const element = React.createElement("h1",{id:"first", className:"Rahul", style:{backgroundColor:"blue", fontSize:"30px", color:"pink"}},"Hello Coder Army");
+const element2 = React.createElement("h2",{id:"second", className:"Rahul", style:{backgroundColor:"black", fontSize:"30px", color:"pink"}},"Maja aaya muje");
+
+
+
+//const div1 = React.createElement('div',{},[element,element2]);
+const Reactroot = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+Reactroot.render(element);
+// Reactroot.render(element2);
+
+
+//Reactroot.render(div1);
+
+
+
+
+
+
+// version: 18.2.3
+
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+// function subtract(a,b){
+//     return a-b;
+// }
+
+
+
+
+
+// version: 19.0.0
+
+// function sum(a,b,c){
+//     return a+b+c;
+// }
+
+// function subtract(a,b){
+//     return a-b;
+// }
+
+// sum(2,3,0);
+
+
+
+
+
