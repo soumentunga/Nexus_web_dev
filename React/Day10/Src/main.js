196c8de5e9b6010e1a9fa5267e9454a4cf220523
[SoumenTunga] <soumentungamail@gmail.com>
2025-04-14 23:03:47 +0530

    Day 10 updated

---
+++ React/Day10/Src/main.js
@@ -0,0 +1,45 @@
+import React, { useState,useEffect, useCallback } from "react"
+import ReactDom from "react-dom/client"
+
+
+function App(){
+
+    const [count,setcount]=useState(0)
+    const [number,setnumber]=useState("")
+
+    
+
+    function Fibonacci(n){
+        if(n<=1)return n
+
+        return Fibonacci(n-1)+Fibonacci(n-2)
+    }
+    const result = Fibonacci(number)
+
+    return(
+        <>
+            <h1>Counter:{count}</h1>
+            <button onClick={()=>setcount(count+1)}>Increment</button>
+            <button onClick={()=>setcount(count-1)}>Decrement</button>
+
+
+            <div>
+
+                <h2>Fibonacci Number :{result}</h2>
+                <input value={number} placeholder=" Enter The n-th term (Fn)
+" onChange={(e)=>setnumber(e.target.value)}></input>
+            </div>
+
+
+
+        
+        </>
+    )
+}
+
+
+
+
+
+
+ReactDom.createRoot(document.getElementById('root')).render(<App></App>)
